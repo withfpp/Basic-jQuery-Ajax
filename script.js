68b43d76f239bf6e45022dbbe27b2b507db60325
teo <teo@tk.telecom>
2014-07-03 18:10:22 +0900

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('button').click(function(){
+     $.getJSON('data.json', function(data){
+      var output = '<ul>';
+      $.each(data, function(key, val){
+        output += "<li> "+ val.name + "</li>";
+      });
+      output += "</ul>";
+      $(".update").append(output);
+     });
+  });
+});
